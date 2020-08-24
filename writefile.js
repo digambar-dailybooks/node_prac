034603597d6649fa1971dfca72d4f86b96a5286a
digambar-dailybooks <digambar.kadam@dailybooks.in>
2020-08-24 12:33:35 +0530

    file operations

---
+++ writefile.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+
+/*fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});*/
+
+fs.writeFile('mynewfile3.txt', 'This is my text', function (err) {
+    if (err) throw err;
+    console.log('Replaced!');
+  });
